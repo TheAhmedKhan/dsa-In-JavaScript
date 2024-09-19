5c4863afe1e49dba916043e7cfc7c15ad34e2e97
TheAhmedKhan <audacityenterprices@gmail.com>
2024-09-20 05:04:04 +0530

    updated

--- singlylinkedlist.js
+++ singlylinkedlist.js
@@ -1,6 +1,7 @@
 "use strict";
 const prompt=require("prompt-sync")();
 
+//This is going to be our head node 
 class Node {
     constructor(val) {
         this.val=val
@@ -118,89 +119,67 @@ class SinglyLinkedList {
         temp.val=val
         console.log(temp)
     }
+    insert(index,val){
+        if(index===0) return this.unshift(val)
+        if(index===this.length) return this.push(val)
+        if(index<0||index>this.length) return false
+        let newNode=new Node(val)
+        let temp=this.head
+        for(let i=0;i<index-1;i++){
+            temp=temp.next
+        }
+        newNode.next=temp.next
+        temp.next=newNode
+        console.log(`New node added at ${index} with value ${newNode.val}`)
+        this.length++
+    }
+    remove(index){
+        if(index===0) return this.shift()
+        if(index===this.length) return this.pop()
+        if(index<0||index>this.length) return false
+        
+        let previousVal=this.head
+        for(let i=0;i<index-1;i++){
+            previousVal=previousVal.next
+        }
+        let nextVal=this.head
+        for(let i=0;i<index;i++){
+            nextVal=nextVal.next
+        }
+        let temp=nextVal
+        temp=null
+        previousVal.next=nextVal.next
+        this.length--
+
+    }
+    reverse(){
+        let temp=this.head
+        this.head=this.tail
+        this.tail=temp
+        let next=temp.next
+        let prev = null
+        
+        for(let i=0; i<this.length;i++){
+            next=temp.next
+            temp.next=prev
+            prev=temp
+            temp=next
+        }
+    }
     
     
     
 }
+
 //Displaying-------------------------------------------------------------------------------------------
 let hvalue=prompt("Enter the Head value : ")
-let first=new SinglyLinkedList(Number(hvalue))
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const options = ['Display', 'Push', 'Pop','Shift','Unshift'];
-let index = 0;
-
-const showMenu = () => {
-  console.clear();
-  options.forEach((option, i) => {
-    if (i === index) {
-        
-      console.log(`> ${option}`);
-    } else {
-      console.log(`  ${option}`);
-    }
-  });
-  if(index==0){
-    first.display()
-    }else if(index==1){
-        let ques=prompt("Do you want to push yes/no only in smallcase : ")
-        if(ques=='yes'){
-            let number = prompt("enter the value : ")
-            first.push(Number(number))
-            console.log('node added')
-        }else if(ques=='no'){
-            return;
-        }
-        
-    }else if(index==2){
-        let ques=prompt("Do you want to pop yes/no only in smallcase : ")
-        if(ques=='yes'){
-            first.pop()
-            console.log('node removed')
-        }else if(ques=='no'){
-            return;
-        }
-    }else if(index==3){
-        let ques=prompt("Do you want to shift yes/no only in smallcase : ")
-        if(ques=='yes'){
-            first.shift()
-            console.log('node removed from head')
-        }else if(ques=='no'){
-            return;
-        }
-    }else if(index==4){
-        let ques=prompt("Do you want to unshift yes/no only in smallcase : ")
-        if(ques=='yes'){
-            let number = prompt("enter the value : ")
-            first.unshift(Number(number))
-            console.log('New head added')
-        }else if(ques=='no'){
-            return;
-        }
-    }
-};
-
-const handleKeyPress = (str, key) => {
-  if (key.name === 'up' && index > 0) {
-    index--;
-  } else if (key.name === 'down' && index < options.length - 1) {
-    index++;
-  } else if (key.name === 'return') {
-    console.log(`You selected: ${options[index]}`);
-    rl.close();
-  }
-  showMenu();
-};
-if(index==0){
-    first.display()
+let first=new SinglyLinkedList(hvalue)
+let question=prompt("what operation do you want to perform 1.push 2.pop 3.ushift 4.shift 5.display 6.get 7.set 8.insert 9.remove 10.reverse : ")
+if(Number(question)===1){
+    let val=prompt("Enter the value to be pushed : ")
+    first.push(val)
+    console.log("Pushed successfully")
+    question=prompt("what operation do you want to perform 1.push 2.pop 3.ushift 4.shift 5.display 6.get 7.set 8.insert 9.remove 10.reverse : ")
+   
 }
-
-rl.input.on('keypress', handleKeyPress);
-showMenu();
 //------------------------------------------------------------------------------------------------------------
